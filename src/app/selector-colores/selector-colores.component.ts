084c1ed6b31a48394e6735efdc1aa7723fd6847c
JuanOrozco46 <juanma4647@gmail.com>
2026-05-15 11:51:12 -0500

    Adelanto del primer proyecto Angular

---
+++ src/app/selector-colores/selector-colores.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-colores',
+  standalone: false,
+  templateUrl: './selector-colores.component.html',
+  styleUrl: './selector-colores.component.scss'
+})
+export class SelectorColoresComponent {
+
+}
